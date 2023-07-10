ba3ab0fe1337517d4f87827f85e0c7374b5c7963
Robert Cabrera <cabrera.rch@gmail.com>
2023-07-10 16:45:23 +0200

    feat: create GET controller and add it to routes

--- config/routes.js
+++ config/routes.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/products.controller");
 
-/* ROUTES */
+/* HOME */
 router.get("/", (req, res, next) => res.status(200).json({ ok: true }));
+
+/* PRODUCTS */
 router.post("/product/new", productController.create);
+router.get("/product/:id", productController.detail);
 
 module.exports = router;
--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -2,9 +2,14 @@ const Product = require("../models/Product.model");
 
 module.exports.create = (req, res, next) => {
   const product = ({ title, description, price } = req.body);
-  console.log(product);
 
   Product.create(product)
     .then((newProduct) => res.status(200).json(newProduct))
+    .catch(next);
+};
+
+module.exports.detail = (req, res, next) => {
+  Product.findById(req.params.id)
+    .then((product) => res.status(200).json(product))
     .catch(next);
 };
